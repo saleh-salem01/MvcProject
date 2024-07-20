d67127fd9e455b9624c7a9db10d000b4b38b5567
saleh-salem <s.salem.1042000@gmail.com>
2024-07-20 18:34:26 +0300

    AddAndEdit

--- Controllers/InstructorController.cs
+++ Controllers/InstructorController.cs
@@ -6,10 +6,12 @@ namespace MvcProject.Controllers
     public class InstructorController : Controller
     {
         Instructor instructor = new Instructor();
+
+        #region ReturnActions
         public IActionResult ShowInstructor(int id)
         {
-            Instructor Ins = instructor.GetInstructor(id);   
-            return View("ShowInstructorView",Ins);
+            Instructor Ins = instructor.GetInstructor(id);
+            return View("ShowInstructorView", Ins);
         }
         ApplicationDbContext context = new ApplicationDbContext();
         public IActionResult ShowAllInstructors()
@@ -18,21 +20,59 @@ namespace MvcProject.Controllers
             List<Instructor> Inses = instructor.GetAllInstructors();
             return View("ShowAllInstructorsView", Inses);
         }
+        #endregion
 
+        #region AddingAction
         public IActionResult NewIns()
         {
-            return View();
+            return View(new Instructor());
         }
-        public IActionResult SaveChanges(Department department) {
-            if (department.Name != null) {
-                context.Instructors.Add(instructor);
+        [HttpPost]
+        public IActionResult SaveChanges(Instructor Ins)
+        {
+            if (Ins.Name != null)
+            {
+                context.Instructors.Add(Ins);
                 context.SaveChanges();
                 List<Instructor> Inses = instructor.GetAllInstructors();
                 return View("ShowAllInstructorsView", Inses);
             }
-            
-            return View("NewIns");
+
+            return View("NewIns", Ins);
         }
+        #endregion
+
+        #region EditAction
+        public IActionResult Edit(int id)
+        {
+            Instructor Ins = context.Instructors.FirstOrDefault(x => x.Id == id);
+            return View("EditIns", Ins);
+        }
+        public IActionResult SaveEdit(int id,Instructor NewIns)
+        {
+            Instructor oldIns = context.Instructors.FirstOrDefault(x => x.Id == id);
+            if (NewIns == null || NewIns.Name == null)
+            {
+                // Handle the case where Ins is null or Ins.Name is null
+                return View("EditIns", NewIns);
+            }
+            else {     
+                if (oldIns != null)
+                {
+                    oldIns.Name = NewIns.Name;
+                    oldIns.Address = NewIns.Address;
+                    oldIns.Salary = NewIns.Salary;
+                    oldIns.Dept_id = NewIns.Dept_id;
+                    oldIns.CrsId = NewIns.CrsId;
+                    context.SaveChanges();
+                   
+                }
+            }
+            // List<Instructor> Inses = instructor.GetAllInstructors();
+            return RedirectToAction("ShowAllInstructors");
+        }
+    }
+        #endregion
 
     }
-}
+
--- obj/Debug/net8.0/MvcProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79e85c9a2fd4625d3e574eda597fe675670538a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7581cb67e144fb6bd0452295b6ead6569c48fbb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
