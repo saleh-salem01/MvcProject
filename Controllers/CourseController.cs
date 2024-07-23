4aa66621760e7f7f361e664792963800d41bf709
saleh-salem <s.salem.1042000@gmail.com>
2024-07-23 16:58:51 +0300

    Add Layoute

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -11,11 +11,41 @@ namespace MvcProject.Controllers
             var s = course.GetCourse(id);
             return View("ShowCourseView",s);
         }
-        public IActionResult ShowAllCourses(int id) {
+        public IActionResult ShowAllCourses() {
             List<Course> ListOFCourses = course.GetAllCourses();
 
             return View("ShowAllCourses",ListOFCourses);
         }
-        
+
+        ApplicationDbContext _context = new ApplicationDbContext();
+
+        #region AddingAction
+        public IActionResult NewCourse()
+        {
+            var Departments = _context.Departments.Select(d=> new
+            {
+                Value = d.Id.ToString(),
+                Text = d.Name
+            }).ToList();
+            ViewBag.Departments = Departments;
+            return View("NewCourseView",new Course());
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult SaveCourseChanges(Course Crs)
+        {
+            if (Crs.Name != null)
+            {
+                _context.Courses.Add(Crs);
+                _context.SaveChanges();
+                
+                return RedirectToAction("ShowAllCourses");
+            }
+
+            return View("NewCourseView",Crs);
+        }
+        #endregion
+
+
     }
 }
